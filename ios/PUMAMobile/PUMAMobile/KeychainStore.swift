3ba6fee2755d75599a432ed980671a1f15d569d6
5cwh4m4t8r-hash <5cwh4m4t8r@privaterelay.appleid.com>
2026-09-24 19:14:12 +0900

    Store PUMA pair token in iOS Keychain

---
+++ ios/PUMAMobile/PUMAMobile/KeychainStore.swift
@@ -0,0 +1,45 @@
+import Foundation
+import Security
+
+enum KeychainStore {
+    static let service = "com.pumastock.mobile"
+    static let tokenAccount = "pair-token"
+
+    static func saveToken(_ token: String) {
+        let data = Data(token.utf8)
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: tokenAccount,
+        ]
+        SecItemDelete(query as CFDictionary)
+        var add = query
+        add[kSecValueData as String] = data
+        add[kSecAttrAccessible as String] = kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly
+        SecItemAdd(add as CFDictionary, nil)
+    }
+
+    static func loadToken() -> String {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: tokenAccount,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne,
+        ]
+        var result: AnyObject?
+        guard SecItemCopyMatching(query as CFDictionary, &result) == errSecSuccess,
+              let data = result as? Data,
+              let token = String(data: data, encoding: .utf8) else { return "" }
+        return token
+    }
+
+    static func clearToken() {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: tokenAccount,
+        ]
+        SecItemDelete(query as CFDictionary)
+    }
+}
